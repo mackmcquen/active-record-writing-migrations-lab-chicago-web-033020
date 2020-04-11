a603f6b1830c9464d35a31ec54cb22ab8c3b3abc
mack_mcquen <mackmcquen@comcast.net>
2020-04-11 14:20:45 -0500

    Done

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,2 +1,13 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
+    sql = <<-SQL
+    CREATE TABLE IF NOT EXISTS students (
+        name TEXT
+    )
+    SQL
+
+    def change
+        create_table(:students) do |t|
+            t.column :name, :string
+        end
+    end
 end
